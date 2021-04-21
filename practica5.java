68f324339561311878875779abdaad1b2de0b09b
Dante <dantevalles2016@yahoo.com>
2021-04-20 23:39:29 -0300

    practica

---
+++ practica5.java
@@ -0,0 +1,20 @@
+package practica_clases;
+
+public class practica5 {
+
+    enum Talla{mini, mediano, grande, muy_grande};
+
+    public static void main(String[] args){
+
+        Talla s=Talla.mini;
+
+        Talla m=Talla.mediano;
+
+        Talla l=Talla.grande;
+
+        Talla xl=Talla.muy_grande;
+//Tipo enumerado
+        
+    }
+    
+}
