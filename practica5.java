6c871f9169455ab87a7ae39723b8ed0e8b5882f4
Dante <dantevalles2016@yahoo.com>
2021-04-21 10:20:45 -0300

    practica

--- practica5.java
+++ practica5.java
@@ -1,18 +1,35 @@
 package practica_clases;
-
+import java.util.*;
 public class practica5 {
 
-    enum Talla{mini, mediano, grande, muy_grande};
+    //enum Talla{mini, mediano, grande, muy_grande};
 
+    enum Talla{
+
+        mini("s"), mediano("m"), grande("l"), muy_grande("xl");
+
+        private Talla(String abreviatura){
+
+            this.abreviatura=abreviatura;
+        }
+
+        public String dameAbreviatura(){
+            return abreviatura;
+        }
+        private String abreviatura;
+            
+    }
     public static void main(String[] args){
 
-        Talla s=Talla.mini;
+      
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Coloca la talla: mini, mediano, grande. muy_grande");
+        String entrada_datos=entrada.next().toLowerCase();
 
-        Talla m=Talla.mediano;
+        Talla la_talla=Enum.valueOf(Talla.class, entrada_datos);
 
-        Talla l=Talla.grande;
-
-        Talla xl=Talla.muy_grande;
+        System.out.println("talla: " + la_talla);
+        System.out.println("abreviatura: " + la_talla.dameAbreviatura());
 //Tipo enumerado
         
     }
