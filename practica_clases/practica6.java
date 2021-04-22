2b04851923e6fffc4de6702114c5a8c8a67ce045
Dante <dantevalles2016@yahoo.com>
2021-04-22 01:23:49 -0300

    practica

---
+++ practica_clases/practica6.java
@@ -0,0 +1,39 @@
+package practica_clases;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+import java.awt.event.*;
+import java.util.*;
+
+public class practica6 {
+
+    public static void main(String[] args){
+
+        DamelaHora oyente=new DamelaHora();
+
+        Timer miTemporizador=new Timer(5000, oyente);
+
+        miTemporizador.start();
+
+        JOptionPane.showMessageDialog(null, "Pulsa aceptar para detener");
+
+        System.exit(0);
+
+
+    }
+    
+}
+
+class DamelaHora implements ActionListener{
+
+    public void actionPerformed(ActionEvent e){
+
+        Date ahora=new Date();
+
+        System.out.println("te doy la hora " + ahora);
+
+        
+
+
+
+    }
+}
